9fbe34ba421105253bd263e432e0c063b8bca651
Marcio Cunha <marcioabcd11@gmail.com>
2022-03-11 15:38:42 -0300

    Preparando Review

--- src/utils/changeDate.js
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs"
-
-export default function changeDate(array, objectKey) {
-
-  const newArr = array.map(el => { return { ...el, [objectKey]: dayjs(el[objectKey]).format('YYYY-MM-DD') } })
-
-  return newArr
-
-}
